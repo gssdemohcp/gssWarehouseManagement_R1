2bebb5f44b0da3526d766d22682287aefbe81aae
S Srinivasan <srinivas129221@gmail.com>
2017-11-22 12:22:41 +0000

    change in putaway - Srini

--- webapp/controller/putAway.controller.js
+++ webapp/controller/putAway.controller.js
@@ -17,7 +17,7 @@ sap.ui.define([
 			this._router = this.getRouter();
 			this._router.getRoute("putaway").attachPatternMatched(this._routePatternMatched, this);
 			this.inputDetails();
-
+			var test;
 		},
 
 		_routePatternMatched: function(oEvent) {
