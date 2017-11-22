c47090c40bf356c4e4483fe502a242c7ba355088
selvan Selvan <mcselvan@gmail.com>
2017-11-22 12:17:02 +0000

    removed test var from filter selvan

--- webapp/model/Filters.js
+++ webapp/model/Filters.js
@@ -24,7 +24,6 @@ sap.ui.define(["sap/ui/base/Object",
 		},
 		
 		getFilters: function(field, value) {
-			var test;
 			var aFilter = new Filter(field, FilterOperator.EQ, value);
 			return aFilter;
 		},
